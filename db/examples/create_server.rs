40fe5134b5f096ca125d9cc7bacc21151c4f865b
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-11-05 15:56:14 +0100

    feat(server): add support for uuids
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- db/examples/create_server.rs
+++ db/examples/create_server.rs
@@ -1,11 +1,12 @@
 use std::sync::Arc;
 
 use db::{database::Database, models::server::CreateServer, repositories};
+use uuid::Uuid;
 
 #[tokio::main]
 pub async fn main() {
     const DATABASE_URL: &str = "postgres://postgres:password@localhost/communities";
-
+    let owner_id = Uuid::parse_str("64811df3-243e-4833-a235-88c2b2726b06").unwrap();
     // Create a new database connection
     let db = Arc::new(Database::new(DATABASE_URL).await.unwrap());
     // Use the database connection (e.g., create a new server)
@@ -15,7 +16,7 @@ pub async fn main() {
         banner_url: None,
         picture_url: None,
         description: Some("This is my server".to_string()),
-        owner_id: "64811df3-243e-4833-a235-88c2b2726b06".to_string(),
+        owner_id,
     };
 
     let created_server = server_repo.create(new_server).await.unwrap();

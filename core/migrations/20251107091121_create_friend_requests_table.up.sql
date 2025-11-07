2d0041ac43d8301061b1c08d50ba62cda8dac34e
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-11-07 10:22:22 +0100

    feat: add friends and friend requests SQL tables
    Signed-off-by: Baptiste Bronsin <baptiste.bronsin@outlook.com>

---
+++ core/migrations/20251107091121_create_friend_requests_table.up.sql
@@ -0,0 +1,7 @@
+-- Create the friend_requests table
+CREATE TABLE friend_requests (
+    user_id_requested UUID NOT NULL,
+    user_id_invited UUID NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (user_id_requested, user_id_invited)
+);

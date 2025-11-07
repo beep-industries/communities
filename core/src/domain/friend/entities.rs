2cfc05f813d6fa61b4bab4876cdea0b4a39ebf94
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-11-07 10:22:49 +0100

    feat: add friends and friend requests models
    Signed-off-by: Baptiste Bronsin <baptiste.bronsin@outlook.com>

---
+++ core/src/domain/friend/entities.rs
@@ -0,0 +1,96 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct UserId(pub Uuid);
+
+impl std::fmt::Display for UserId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl From<Uuid> for UserId {
+    fn from(uuid: Uuid) -> Self {
+        UserId(uuid)
+    }
+}
+
+impl From<UserId> for Uuid {
+    fn from(user_id: UserId) -> Self {
+        user_id.0
+    }
+}
+
+// Is used to map database rows to domain entities
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FriendRow {
+    pub user_id_1: UserId,
+    pub user_id_2: UserId,
+
+    pub created_at: DateTime<Utc>,
+}
+
+// Is used in API responses
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Friend {
+    pub user_id_1: UserId,
+    pub user_id_2: UserId,
+
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InsertFriendInput {
+    pub user_id_1: UserId,
+    pub user_id_2: UserId,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DeleteFriendInput {
+    pub user_id_1: UserId,
+    pub user_id_2: UserId,
+}
+
+// Is used to map database rows to domain entities
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FriendRequestRow {
+    pub user_id_requested: UserId,
+    pub user_id_invited: UserId,
+
+    pub created_at: DateTime<Utc>,
+}
+
+// Is used in API responses
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FriendRequest {
+    pub user_id_requested: UserId,
+    pub user_id_invited: UserId,
+
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InsertFriendRequestInput {
+    pub user_id_requested: UserId,
+    pub user_id_invited: UserId,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AcceptFriendRequestInput {
+    pub user_id_requested: UserId,
+    pub user_id_invited: UserId,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DeclineFriendRequestInput {
+    pub user_id_requested: UserId,
+    pub user_id_invited: UserId,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DeleteFriendRequestInput {
+    pub user_id_requested: UserId,
+    pub user_id_invited: UserId,
+}

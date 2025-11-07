61a7ee51b1557c4033acccd9ac71396fbe407768
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-11-07 10:42:23 +0100

    Fix: Add status attribute in friend requests table

--- core/migrations/20251107091121_create_friend_requests_table.up.sql
+++ core/migrations/20251107091121_create_friend_requests_table.up.sql
@@ -2,6 +2,7 @@
 CREATE TABLE friend_requests (
     user_id_requested UUID NOT NULL,
     user_id_invited UUID NOT NULL,
+    status VARCHAR(10) DEFAULT 'pending' NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (user_id_requested, user_id_invited)
 );

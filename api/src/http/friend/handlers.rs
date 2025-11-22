4896a10524f92ec7cf518131c62dbf111985d6cd
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-11-22 21:51:36 +0100

    Feat: Add delete friendship

--- api/src/http/friend/handlers.rs
+++ api/src/http/friend/handlers.rs
@@ -1,6 +1,7 @@
-use core::domain::{common::GetPaginated, friend::{entities::{Friend, UserId}, ports::FriendService}};
+use core::domain::{common::GetPaginated, friend::{entities::{DeleteFriendInput, Friend, UserId}, ports::FriendService}};
 
-use axum::{Extension, extract::{Query, State}};
+use axum::{Extension, extract::{Path, Query, State}};
+use uuid::Uuid;
 
 use crate::http::server::{ApiError, AppState, Response, middleware::AuthState, response::PaginatedResponse};
 
@@ -23,4 +24,17 @@ pub async fn get_friends(
     };
 
     Ok(Response::ok(response))
+}
+
+pub async fn delete_friend(
+    Path(friend_id): Path<Uuid>,
+    State(state): State<AppState>,
+    Extension(auth_state): Extension<AuthState>
+) -> Result<Response<()>, ApiError> {
+    let user_id = UserId::from(auth_state.user_id);
+    let friend_id = UserId::from(friend_id);
+
+    state.service.delete_friend(DeleteFriendInput { user_id_1: user_id, user_id_2: friend_id }).await?;
+
+    Ok(Response::ok(()))
 }
--- api/src/http/friend/routes.rs
+++ api/src/http/friend/routes.rs
@@ -1,8 +1,9 @@
-use axum::{Router, routing::get};
+use axum::{Router, routing::{delete, get}};
 
-use crate::http::{friend::handlers::get_friends, server::AppState};
+use crate::http::{friend::handlers::{delete_friend, get_friends}, server::AppState};
 
 pub fn friend_routes() -> Router<AppState> {
     Router::new()
         .route("/friends", get(get_friends))
+        .route("/friends/{friend_id}", delete(delete_friend))
 }

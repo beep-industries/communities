545664525409d1b83af44c930b1c37f006b31686
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-11-25 15:32:27 +0100

    add more tests

--- core/src/domain/test/friend/mock_test.rs
+++ core/src/domain/test/friend/mock_test.rs
@@ -1,3 +1,4 @@
+// == Friend requests ==
 
 #[cfg(test)]
 #[tokio::test]
@@ -141,6 +142,245 @@ async fn test_accept_friend_requests_success() -> Result<(), Box<dyn std::error:
         .expect("list_requests returned an error");
 
     assert_eq!(friend_requests.0.len(), 0, "Expected no friend requests in the list after acceptance");
+
+    Ok(())
+}
+
+#[cfg(test)]
+#[tokio::test]
+async fn test_accept_friend_requests_fail() -> Result<(), Box<dyn std::error::Error>> {
+    use crate::{Service, domain::{friend::{entities::UserId, ports::{FriendRequestService, MockFriendshipRepository}}, health::port::MockHealthRepository, server::ports::MockServerRepository}};
+
+    let server_mock_repo = MockServerRepository::new();
+    let friend_mock_repo = MockFriendshipRepository::new();
+    let health_mock_repo = MockHealthRepository::new();
+    let service = Service::new(server_mock_repo, friend_mock_repo.clone(), health_mock_repo);
+
+    let user_id_requested = UserId::from("123e4567-e89b-12d3-a456-426614174001".to_string());
+    let user_id_invited = UserId::from("123e4567-e89b-12d3-a456-426614174002".to_string());
+
+    // Test the accept_friend_request method
+    let error = service.accept_friend_request(&user_id_requested, &user_id_invited)
+        .await
+        .expect_err("accept_friend_request should have returned an error");
+
+    assert_eq!(error.to_string(), format!("Failed to remove friendship between {} and {}", user_id_invited, user_id_requested), "Expected duplicate friend request error");
+
+    Ok(())
+}
+
+#[cfg(test)]
+#[tokio::test]
+async fn test_decline_friend_requests_success() -> Result<(), Box<dyn std::error::Error>> {
+    use crate::{Service, domain::{friend::{entities::UserId, ports::{FriendRequestService, FriendshipRepository, MockFriendshipRepository}}, health::port::MockHealthRepository, server::ports::MockServerRepository}};
+
+    let server_mock_repo = MockServerRepository::new();
+    let friend_mock_repo = MockFriendshipRepository::new();
+    let health_mock_repo = MockHealthRepository::new();
+    let service = Service::new(server_mock_repo, friend_mock_repo.clone(), health_mock_repo);
+
+    let user_id_requested = UserId::from("123e4567-e89b-12d3-a456-426614174001".to_string());
+    let user_id_invited = UserId::from("123e4567-e89b-12d3-a456-426614174002".to_string());
+
+    // Add dataset
+    friend_mock_repo.create_request(&user_id_requested, &user_id_invited)
+        .await
+        .expect("create_request returned an error");
+
+    // Test the decline_friend_request method
+    let friend_request = service.decline_friend_request(&user_id_requested, &user_id_invited)
+        .await
+        .expect("decline_friend_request returned an error");
+
+    assert_eq!(friend_request.user_id_requested.to_string(), user_id_requested.to_string(), "Expected same requested user ID");
+    assert_eq!(friend_request.user_id_invited.to_string(), user_id_invited.to_string(), "Expected same invited user ID");
+    assert_eq!(friend_request.status, 1, "Expected status to be 1 (refused)");
+
+    Ok(())
+}
+
+#[cfg(test)]
+#[tokio::test]
+async fn test_decline_friend_requests_fail() -> Result<(), Box<dyn std::error::Error>> {
+    use crate::{Service, domain::{friend::{entities::UserId, ports::{FriendRequestService, MockFriendshipRepository}}, health::port::MockHealthRepository, server::ports::MockServerRepository}};
+
+    let server_mock_repo = MockServerRepository::new();
+    let friend_mock_repo = MockFriendshipRepository::new();
+    let health_mock_repo = MockHealthRepository::new();
+    let service = Service::new(server_mock_repo, friend_mock_repo.clone(), health_mock_repo);
+
+    let user_id_requested = UserId::from("123e4567-e89b-12d3-a456-426614174001".to_string());
+    let user_id_invited = UserId::from("123e4567-e89b-12d3-a456-426614174002".to_string());
+
+    // Test the decline_friend_request method
+    let error = service.decline_friend_request(&user_id_requested, &user_id_invited)
+        .await
+        .expect_err("decline_friend_request should have returned an error");
+
+        assert_eq!(error.to_string(), format!("Failed to remove friendship between {} and {}", user_id_invited, user_id_requested), "Expected duplicate friend request error");
+
+    Ok(())
+}
+
+#[cfg(test)]
+#[tokio::test]
+async fn test_delete_friend_requests_success() -> Result<(), Box<dyn std::error::Error>> {
+    use crate::{Service, domain::{friend::{entities::UserId, ports::{FriendRequestService, FriendshipRepository, MockFriendshipRepository}}, health::port::MockHealthRepository, server::ports::MockServerRepository}};
+
+    let server_mock_repo = MockServerRepository::new();
+    let friend_mock_repo = MockFriendshipRepository::new();
+    let health_mock_repo = MockHealthRepository::new();
+    let service = Service::new(server_mock_repo, friend_mock_repo.clone(), health_mock_repo);
+
+    let user_id_requested = UserId::from("123e4567-e89b-12d3-a456-426614174001".to_string());
+    let user_id_invited = UserId::from("123e4567-e89b-12d3-a456-426614174002".to_string());
+
+    // Add dataset
+    friend_mock_repo.create_request(&user_id_requested, &user_id_invited)
+        .await
+        .expect("create_request returned an error");
+
+    // Test the delete_friend_request method
+    service.delete_friend_request(&user_id_requested, &user_id_invited)
+        .await
+        .expect("delete_friend_request returned an error");
+
+    Ok(())
+}
+
+#[cfg(test)]
+#[tokio::test]
+async fn test_delete_friend_requests_fail() -> Result<(), Box<dyn std::error::Error>> {
+    use crate::{Service, domain::{friend::{entities::UserId, ports::{FriendRequestService, MockFriendshipRepository}}, health::port::MockHealthRepository, server::ports::MockServerRepository}};
+
+    let server_mock_repo = MockServerRepository::new();
+    let friend_mock_repo = MockFriendshipRepository::new();
+    let health_mock_repo = MockHealthRepository::new();
+    let service = Service::new(server_mock_repo, friend_mock_repo.clone(), health_mock_repo);
+
+    let user_id_requested = UserId::from("123e4567-e89b-12d3-a456-426614174001".to_string());
+    let user_id_invited = UserId::from("123e4567-e89b-12d3-a456-426614174002".to_string());
+
+    // Test the delete_friend_request method
+    let error = service.delete_friend_request(&user_id_requested, &user_id_invited)
+        .await
+        .expect_err("delete_friend_request should have returned an error");
+
+        assert_eq!(error.to_string(), format!("Failed to remove friendship between {} and {}", user_id_invited, user_id_requested), "Expected duplicate friend request error");
+
+    Ok(())
+}
+
+// == Friends ==
+
+#[cfg(test)]
+#[tokio::test]
+async fn test_get_friends_success() -> Result<(), Box<dyn std::error::Error>> {
+    use crate::{Service, domain::{common::GetPaginated, friend::{entities::UserId, ports::{FriendService, FriendshipRepository, MockFriendshipRepository}}, health::port::MockHealthRepository, server::ports::MockServerRepository}};
+
+    let server_mock_repo = MockServerRepository::new();
+    let friend_mock_repo = MockFriendshipRepository::new();
+    let health_mock_repo = MockHealthRepository::new();
+    let service = Service::new(server_mock_repo, friend_mock_repo.clone(), health_mock_repo);
+
+    let user_id_requested = UserId::from("123e4567-e89b-12d3-a456-426614174001".to_string());
+    let user_id_invited = UserId::from("123e4567-e89b-12d3-a456-426614174002".to_string());
+
+    // Add dataset
+    friend_mock_repo.create_request(&user_id_requested, &user_id_invited).await?;
+    friend_mock_repo.accept_request(&user_id_requested, &user_id_invited).await?;
+    
+    // Test the get_friends method
+    let friends1 = service.get_friends(&GetPaginated::default(), &user_id_requested)
+        .await
+        .expect("get_friends returned an error");
+
+    assert_eq!(friends1.0.len(), 1, "Expected one friend in the list");
+    assert_eq!(friends1.1, 1, "Expected total count to be 1");
+
+    Ok(())
+}
+
+#[cfg(test)]
+#[tokio::test]
+async fn test_get_friends_success_with_pagination() -> Result<(), Box<dyn std::error::Error>> {
+    use crate::{Service, domain::{common::GetPaginated, friend::{entities::UserId, ports::{FriendService, FriendshipRepository, MockFriendshipRepository}}, health::port::MockHealthRepository, server::ports::MockServerRepository}};
+
+    let server_mock_repo = MockServerRepository::new();
+    let friend_mock_repo = MockFriendshipRepository::new();
+    let health_mock_repo = MockHealthRepository::new();
+    let service = Service::new(server_mock_repo, friend_mock_repo.clone(), health_mock_repo);
+
+    let user_id_requested = UserId::from("123e4567-e89b-12d3-a456-426614174001".to_string());
+    let user_id_invited = UserId::from("123e4567-e89b-12d3-a456-426614174002".to_string());
+    
+    // Add dataset
+    friend_mock_repo.create_request(&user_id_requested, &user_id_invited).await?;
+    friend_mock_repo.accept_request(&user_id_requested, &user_id_invited).await?;
+    let pagination = GetPaginated { page: 2, limit: 10 };
+
+    // Test the get_friends method
+    let friends1 = service.get_friends(&pagination, &user_id_requested)
+        .await
+        .expect("get_friends returned an error");
+
+    assert_eq!(friends1.0.len(), 0, "Expected no friends in the list");
+    assert_eq!(friends1.1, 1, "Expected total count to be 1");
+
+    let friends2 = service.get_friends(&pagination, &user_id_invited)
+        .await
+        .expect("get_friends returned an error");
+
+    assert_eq!(friends2.0.len(), 0, "Expected no friends in the list");
+    assert_eq!(friends2.1, 1, "Expected total count to be 1");
+
+    Ok(())
+}
+
+#[cfg(test)]
+#[tokio::test]
+async fn test_delete_friend_success() -> Result<(), Box<dyn std::error::Error>> {
+    use crate::{Service, domain::{friend::{entities::{DeleteFriendInput, UserId}, ports::{FriendService, FriendshipRepository, MockFriendshipRepository}}, health::port::MockHealthRepository, server::ports::MockServerRepository}};
+
+    let server_mock_repo = MockServerRepository::new();
+    let friend_mock_repo = MockFriendshipRepository::new();
+    let health_mock_repo = MockHealthRepository::new();
+    let service = Service::new(server_mock_repo, friend_mock_repo.clone(), health_mock_repo);
+
+    let user_id_requested = UserId::from("123e4567-e89b-12d3-a456-426614174001".to_string());
+    let user_id_invited = UserId::from("123e4567-e89b-12d3-a456-426614174002".to_string());
+
+    // Add dataset
+    friend_mock_repo.create_request(&user_id_requested, &user_id_invited).await?;
+    friend_mock_repo.accept_request(&user_id_requested, &user_id_invited).await?;
+
+    // Test the delete_friend_request method
+    service.delete_friend(DeleteFriendInput { user_id_1: user_id_requested, user_id_2: user_id_invited })
+        .await
+        .expect("delete_friend_request returned an error");
+
+    Ok(())
+}
+
+#[cfg(test)]
+#[tokio::test]
+async fn test_delete_friend_fail() -> Result<(), Box<dyn std::error::Error>> {
+    use crate::{Service, domain::{friend::{entities::{DeleteFriendInput, UserId}, ports::{FriendService, MockFriendshipRepository}}, health::port::MockHealthRepository, server::ports::MockServerRepository}};
+
+    let server_mock_repo = MockServerRepository::new();
+    let friend_mock_repo = MockFriendshipRepository::new();
+    let health_mock_repo = MockHealthRepository::new();
+    let service = Service::new(server_mock_repo, friend_mock_repo.clone(), health_mock_repo);
+
+    let user_id_requested = UserId::from("123e4567-e89b-12d3-a456-426614174001".to_string());
+    let user_id_invited = UserId::from("123e4567-e89b-12d3-a456-426614174002".to_string());
+
+    // Test the delete_friend method
+    let error = service.delete_friend(DeleteFriendInput { user_id_1: user_id_requested, user_id_2: user_id_invited })
+        .await
+        .expect_err("delete_friend should have returned an error");
+
+        assert_eq!(error.to_string(), format!("Failed to remove friendship between {} and {}", user_id_requested, user_id_invited), "Expected duplicate friend request error");
 
     Ok(())
 }

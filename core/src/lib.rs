fe55dee6b939bbf2ad29676f680678d024ae3d74
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-11-27 19:10:35 +0100

    feat: integration testing, repository management, app state builder, test context
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,7 +3,7 @@ pub mod domain;
 pub mod infrastructure;
 
 // Re-export commonly used types for convenience
-pub use application::{CommunitiesService, create_service};
+pub use application::{CommunitiesService, create_repositories};
 pub use domain::common::services::Service;
 pub use infrastructure::friend::repositories::postgres::PostgresFriendshipRepository;
 pub use infrastructure::health::repositories::postgres::PostgresHealthRepository;

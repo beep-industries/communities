8a59f23a05681eaa7b1657cb85c2bb2c48248e7e
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2026-01-17 22:02:58 +0100

    feat: route for roles
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- api/src/http/role/routes.rs
+++ api/src/http/role/routes.rs
@@ -1,1 +1,18 @@
+use utoipa_axum::{router::OpenApiRouter, routes};
 
+use crate::{
+    AppState,
+    http::role::handlers::{
+        __path_create_role, __path_delete_role, __path_get_role, __path_list_roles_by_server,
+        __path_update_role, create_role, delete_role, get_role, list_roles_by_server, update_role,
+    },
+};
+
+pub fn role_routes() -> OpenApiRouter<AppState> {
+    OpenApiRouter::new()
+        .routes(routes!(create_role))
+        .routes(routes!(get_role))
+        .routes(routes!(update_role))
+        .routes(routes!(list_roles_by_server))
+        .routes(routes!(delete_role))
+}

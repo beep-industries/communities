46fb1ba1ffd5531ee1d2bf1904af6099f459d70f
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2026-01-25 10:06:38 +0100

    fix: add the routing back to allow member to join public server
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- api/src/http/server_members/routes.rs
+++ api/src/http/server_members/routes.rs
@@ -1,15 +1,16 @@
 use utoipa_axum::{router::OpenApiRouter, routes};
 
-use crate::{
-    http::server::AppState,
-    http::server_members::handlers::{
-        __path_delete_member, __path_list_members, __path_update_member, delete_member,
-        list_members, update_member,
+use crate::http::{
+    server::AppState,
+    server_members::handlers::{
+        __path_create_member, __path_delete_member, __path_list_members, __path_update_member,
+        create_member, delete_member, list_members, update_member,
     },
 };
 
 pub fn server_member_routes() -> OpenApiRouter<AppState> {
     OpenApiRouter::new()
+        .routes(routes!(create_member))
         .routes(routes!(list_members))
         .routes(routes!(update_member))
         .routes(routes!(delete_member))
